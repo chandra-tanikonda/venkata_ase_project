c081df820d00be091f86429e3f09d257c6c89f16
Chandra <tatikondapurna@gmail.com>
2024-11-23 12:39:12 -0600

    minor changes

--- employee_attendance_system/employee_application/management/commands/qr_code.py
+++ employee_attendance_system/employee_application/management/commands/qr_code.py
@@ -1,13 +1,9 @@
 import cv2
 import openpyxl
-import pyzbar.pyzbar as pyzbar
 import webbrowser
 from django.core.management.base import BaseCommand
 
-from django.utils import timezone
-from employee_application.models import Employee,EmployeeAttendanceRecord
 import cv2
-import pyzbar.pyzbar as pyzbar
 import webbrowser
 from django.utils import timezone
 from employee_application.models import Employee, EmployeeAttendanceRecord
@@ -18,6 +14,9 @@ def open_web_browser_for_qr_code_scan():
     When a QR code is detected, it opens the URL encoded in it in a web browser,
     and updates or creates an attendance record for the associated employee.
     """
+    # Initialize the QRCode detector
+    qr_detector = cv2.QRCodeDetector()
+
     # Start capturing video from the webcam
     video_capture = cv2.VideoCapture(0)
 
@@ -39,19 +38,15 @@ def open_web_browser_for_qr_code_scan():
         # Display the captured frame in a window named 'frame'
         cv2.imshow("frame", video_frame)
 
-        # Attempt to decode any QR codes in the frame
-        videoObjects = pyzbar.decode(video_frame)
+        # Attempt to detect and decode a QR code in the frame
+        qr_value, bbox, _ = qr_detector.detectAndDecode(video_frame)
 
-        # Process the decoded QR codes
-        for video_obj in videoObjects:
-            if len(video_obj.data) < 1:
-                continue  # Skip if the QR code data is empty
+        # If a QR code is detected and decoded
+        if qr_value:
+            print("QR Code Value:", qr_value)
 
-            # Decode the QR code data to a string
-            qr_value = video_obj.data.decode('utf-8')
             # Open the decoded URL in a web browser
             webbrowser.open(qr_value)
-            print("QR Code Value:", qr_value)
 
             # Attempt to find an employee with the matching unique QR code
             employee = Employee.objects.filter(employee_identifier=qr_value).first()
@@ -79,15 +74,12 @@ def open_web_browser_for_qr_code_scan():
             return  # Exit the function after handling one QR code
 
         # Wait for 1 ms and check if the ESC key is pressed to exit
-        if cv2.waitKey(1) == 27:
+        if cv2.waitKey(1) == 27:  # ESC key
             break
 
     # Release the webcam and close all OpenCV windows
     video_capture.release()
     cv2.destroyAllWindows()
-
-
-
 
 class Command(BaseCommand):
     help = 'Generate the QR code and scan the QR code value.'
